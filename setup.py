1e088e704243c84b2b96e6223e5b0f9b229839f5
Thomas Hanke <38633260+ThHanke@users.noreply.github.com>
2025-05-13 12:07:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open(path.join(here,"requirements.txt")) as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="""ckanext-chat""",
     # Versions should comply with PEP440.  For a discussion on single-sourcing
@@ -20,6 +23,7 @@ setup(
     description="""Extension adds a pydantic ai chat interface to CKAN, that can run actions with user aware context.""",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=requirements,
     # The project's main homepage.
     url="https://github.com/Mat-O-Lab/ckanext-chat",
     # Author details
